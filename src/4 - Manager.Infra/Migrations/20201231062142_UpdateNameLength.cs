a6fd49334b4e20b3d7565f639afe5284c240d1b6
Audrey Ernesto de Lima <alima@inovvati.com.br>
2024-09-23 18:51:14 -0400

    subindo o projeto

---
+++ src/4 - Manager.Infra/Migrations/20201231062142_UpdateNameLength.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Manager.Infra.Migrations
+{
+    public partial class UpdateNameLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
